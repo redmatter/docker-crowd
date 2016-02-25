a34b1da160a213dd46837ee0b27f4f48c6c069c3
Dino Korah <dino.korah@redmatter.com>
2016-02-25 16:14:40 +0000

    Adding docker-compose YML

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 
 catalina_sh="${CROWD_INSTALL_DIR}/apache-tomcat/bin/catalina.sh"
 
-trap '${catalina_sh} stop' SIGTERM
+stop_catalina() {
+	echo stop_catalina: SIGTERM received;
+	${catalina_sh} stop;
+}
+
+trap 'stop_catalina' SIGTERM
 
 exec ${catalina.sh} run
